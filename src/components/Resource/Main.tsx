bacd3763563a548bdf8b2841bb0e33b2e1c0985a
SauravPatel027 <140683115+SauravPatel027@users.noreply.github.com>
2024-12-27 20:04:04 +0530

    Update Main.tsx

--- src/components/Resource/Main.tsx
+++ src/components/Resource/Main.tsx
@@ -4,7 +4,7 @@ const TimelinePage = ({ profileName }: { profileName: string | undefined }) => {
   const profile = profileData[profileName as keyof typeof profileData];
 
   return (  
-    <div className="w-full bg-[#D9D9D9] p-8">
+    <div className="w-full bg-[#FFFFFF] p-8">
       {profile?.guide?.html && (
         <div
           dangerouslySetInnerHTML={{ __html: profile.guide.html }}
